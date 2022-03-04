dc6472c0a51fd696816c5f21c06c81e449310516
Wonseok <ls4050@naver.com>
2022-03-04 12:54:45 +0900

    react #ex02 ex03 build

---
+++ 1.basics/ex01/public/index.js
@@ -0,0 +1,7 @@
+function App(){
+    const App = document.createElement('h1');
+    App.textContent = 'Hello World';
+    return App;
+}
+
+document.getElementById('root').appendChild(App());
---
+++ 1.basics/ex02/public/App.js
@@ -0,0 +1,5 @@
+function App(){
+    const App = document.createElement('h1');
+    App.textContent = 'Hello World';
+    return App;
+}
