b6c499534bb6904e0529cd30ffd18f465146c6bf
Wonseok <ls4050@naver.com>
2022-03-04 14:51:19 +0900

    react #application 분리

---
+++ 1.basics/ex03/public/App.js
@@ -0,0 +1,7 @@
+const App = function App(){
+    const App = document.createElement('h1');
+    App.textContent = 'Hello World';
+    return App;
+}
+
+export { App }
---
+++ 1.basics/ex04/dev-server.js
@@ -0,0 +1,13 @@
+import http from 'http';
+import * as path from 'path';
+import express from 'express';
+
+const port = 9090;
+const application = express().use(express.static(path.join(path.resolve('.'), 'public')));
+
+http
+    .createServer(application)
+    .on('listening', function(){
+        console.info("http server runs on ${port}");
+    })
+.listen(port);
