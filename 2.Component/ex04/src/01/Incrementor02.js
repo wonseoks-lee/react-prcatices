563fa437f85c85664dc42e7c3bdc6a83c2146378
Wonseok <ls4050@naver.com>
2022-03-17 12:18:23 +0900

    useState return array

--- 2.Component/ex04/src/01/Incrementor02.js
+++ 2.Component/ex04/src/01/Incrementor02.js
@@ -3,14 +3,21 @@ import React, {useState} from 'react';
 export default function ({ begin, step }) {
     const [val, setVal] = useState(begin);
     
+    const r = useState(begin)
+    const { b } = {a: 10, b: 20};
+    const [d,f] = [1,2,3,4];
+
+    console.log(b, d, f)
 
     return (
         <div>
-            <button onClick={e => setVal(val+step)}>
+            {/* <button onClick={e => setVal(val+step)}> */}
+            <button onClick={e => r[1](r[0]+step)}>
                 <strong>+</strong>
             </button>
             {' '}
-            <span>{ val }</span>
+            {/* <span>{ val }</span> */}
+            <span>{ r[0] }</span>
         </div>
     );
 }
