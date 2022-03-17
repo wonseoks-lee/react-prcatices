2aba04fb138e0ebac7aa8b553e592a107ddce932
Wonseok <ls4050@naver.com>
2022-03-17 11:08:00 +0900

    useRef

--- 2.Component/ex03/src/05/App.js
+++ 2.Component/ex03/src/05/App.js
@@ -3,6 +3,7 @@ import './assets/scss/App.scss';
 
 export default function App() {
 
+    const result = Array.from({length: 10}, (_, i) => i + 1)
     
     const outterRef = useRef(null);
     const innerRef = useRef(null);
@@ -10,15 +11,17 @@ export default function App() {
     return (
         <div
             ref={outterRef}
-            className={'App'}>
+            className={'App'}
+            onScroll={ (e) => {console.log(outterRef.current.clientHeight, outterRef.current.scrollTop, innerRef.current.clientHeight)}}>
             <div
                 ref={ innerRef }>
                 <ul>
-                    { Array.from({length: 100}, (_, i) => i+1).map(i =>
-                        <li>
-                            { `아이템 ${i} 입니다.` }
-                        </li>
-                    ) }
+                    { 
+                        Array.from({length: 100}, (_, i) => i + 1).map(e => 
+                            <li key={e}> 
+                                { `아이템 ${e} 입니다.` } 
+                            </li>)
+                    }
                 </ul>
             </div>
         </div>
