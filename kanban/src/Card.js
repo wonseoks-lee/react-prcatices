b1076955b2bfc15f8abea0af3e1daf38d78d58ea
Wonseok <ls4050@naver.com>
2022-03-18 10:07:14 +0900

    오류수정

--- kanban/src/Card.js
+++ kanban/src/Card.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import styles from './assets/scss/Card.scss'
+import styles from './assets/scss/Card.css'
 import TaskList from './TaskList'
 
 const Card = ({card}) => {
     const [showDetails, setShowDetails] = useState(false);
+    return (
         <div className={styles.Card}>
             <div className={styles.Card__Title}>
                 {card.title}
