997c2a29e40e619804e5717686d68969b4a9f630
Wonseok <ls4050@naver.com>
2022-03-21 09:16:43 +0900

    kanban finish

--- kanban/src/Card.js
+++ kanban/src/Card.js
@@ -5,17 +5,14 @@ import TaskList from './TaskList'
 const Card = ({card}) => {
     const [showDetails, setShowDetails] = useState(false);
 
-    const onClickTitle = e => {
-        setShowDetails(showDetails === false ? true : false);
-        console.log(showDetails)
-    }
-
     return (
         <div className={styles.Card} >
-            <div className={styles.Card__Title} onClick={onClickTitle}>{card.title}</div>
+            <div className={showDetails ? [styles.Card__Title, styles.Card__Title__open].join(' ') : styles.Card__Title} 
+                    onClick={e => setShowDetails(!showDetails)}>{card.title}</div>
+            <div className={styles.Card__Details}>{card.description}</div>
             {
                 showDetails === false ? 
-                    <><div className={styles.Card__Details}>{card.description}</div><TaskList tasks={card.tasks} /></> : <div/>
+                    null : <TaskList tasks={card.tasks} />
             }
         </div>
     )
--- kanban/src/Task.js
+++ kanban/src/Task.js
@@ -3,13 +3,18 @@ import styles from './assets/css/Task.css'
 const Task = ({task}) => {
     const [taskDone, setTaskDone] = useState(task.done);
 
+    const onChangetaskDone = (e) => {
+        task.done = !taskDone;
+        setTaskDone(taskDone === false ? true : false); 
+    }
+
     return (
         <li className={styles.TaskList__Task}>
             <input 
                 type='checkbox' 
                 value = {taskDone}
                 checked={taskDone}
-                onChange={e => setTaskDone(taskDone === false ? true : false)}
+                onChange={onChangetaskDone}
                 />  
             {task.name}
             <a href='#' className={styles.TaskList__Task__remove}></a>
