4d59b68fdb093becfd5965d27daff271bd08b5d5
Wonseok <ls4050@naver.com>
2022-03-21 10:53:17 +0900

    props validation

--- 2.Component/ex01/src/04/App.js
+++ 2.Component/ex01/src/04/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import FoodList from './FoodList'
 import MyComponent from './MyComponent'
 
 const App = function() {
     return (
         <div id="App">
-            <MyComponent />
+            <MyComponent 
+                // props01={'문자열'}
+                props02={1000}
+                props03={true}
+                />
         </div>
     )
 }
--- 2.Component/ex01/src/04/MyComponent.js
+++ 2.Component/ex01/src/04/MyComponent.js
@@ -1,11 +1,33 @@
-import React from 'react'
-
-const MyComponent = () => {
+import React, {Fragment} from 'react'
+import {PropTypes} from 'prop-types';
+export default function MyComponent({props01, props02, props03}) {
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <h2>Property Validation</h2>
+            <span>props01: {props01 ? props01 : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>props02: {props02 ? props02 : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>props03: {props03 ? `${props03}` : '--- Not Set ---'}</span>
+            <br/>
+
+        </Fragment>
     )
 }
 
-export default MyComponent
+MyComponent.propTypes = {
+    // PropTypes Validator(primitive)
+    props01: PropTypes.string,
+    props02: PropTypes.number.isRequired,
+    props03: PropTypes.bool.isRequired
+
+}
+
+// default value
+MyComponent.defaultProps = {
+    props01: '기본값',
+    props02: 10,
+    props03: false
+}
