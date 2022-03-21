3848420ed515018acb6d48282986dfb5c9399d1a
Wonseok <ls4050@naver.com>
2022-03-21 11:33:17 +0900

    PropTypes Validator(combined primitive)

--- 2.Component/ex01/src/04/App.js
+++ 2.Component/ex01/src/04/App.js
@@ -8,6 +8,16 @@ const App = function() {
                 // props01={'문자열'}
                 props02={1000}
                 props03={true}
+                props04={{no:1}}
+                props05={[1, 2, 3, 4]}
+                props06={() => '함수'}
+                props07={200}
+                props08={[false, false, true]}
+                props09={{
+                    no: 1,
+                    name: '둘리',
+                    email: 'dooly@gmail.com'
+                }}
                 />
         </div>
     )
--- 2.Component/ex01/src/04/MyComponent.js
+++ 2.Component/ex01/src/04/MyComponent.js
@@ -1,6 +1,6 @@
 import React, {Fragment} from 'react'
 import {PropTypes} from 'prop-types';
-export default function MyComponent({props01, props02, props03}) {
+export default function MyComponent({props01, props02, props03, props04, props05, props06, props07, props08, props09}) {
     return (
         <Fragment>
             <h2>Property Validation</h2>
@@ -13,6 +13,35 @@ export default function MyComponent({props01, props02, props03}) {
             <span>props03: {props03 ? `${props03}` : '--- Not Set ---'}</span>
             <br/>
 
+            <span>props04: {props04 ? props04.no : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>props05: {props05 ? props05.map((e, i) => <b key={i}>{e}</b>) : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>props06: {props06 ? props06() : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>props07: {props07 ? props07 : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>props08: {props08 ? props08.map((e, i) => <b key={i}>{`${e}`}</b>) : '--- Not Set ---'}</span>
+            <br/>
+
+            <span>props09: 
+                {
+                    props09 ? 
+                        <div>
+                            <h3>{props09.no}</h3>
+                            <h4>{props09.name}</h4>
+                            <h5>{props09.email}</h5>
+                        </div> :
+                        '--- Not Set ---'
+                }
+            </span>
+            <br/>
+
+            
         </Fragment>
     )
 }
@@ -21,13 +50,27 @@ MyComponent.propTypes = {
     // PropTypes Validator(primitive)
     props01: PropTypes.string,
     props02: PropTypes.number.isRequired,
-    props03: PropTypes.bool.isRequired
+    props03: PropTypes.bool.isRequired,
+    props04: PropTypes.object.isRequired,
+    props05: PropTypes.array.isRequired,
+    props06: PropTypes.func.isRequired,
 
+    // PropTypes Validator(combined primitive)
+    props07: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    props08: PropTypes.arrayOf(PropTypes.bool).isRequired,
+    props09: PropTypes.shape({
+        no: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired
+    }).isRequired
 }
 
 // default value
 MyComponent.defaultProps = {
     props01: '기본값',
     props02: 10,
-    props03: false
+    props03: false,
+    props04: {},
+    props05: [],
+    props06: () => {} // dummy 함수
 }
