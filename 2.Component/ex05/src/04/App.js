0e4f678a433a8ea6abdade3668fcc1f2fe24368c
Wonseok <ls4050@naver.com>
2022-03-24 01:07:11 +0900

    clock finish

--- 2.Component/ex05/src/04/App.js
+++ 2.Component/ex05/src/04/App.js
@@ -3,13 +3,33 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+    const date = new Date();
+    const hours = date.getHours();
+    const [state, setState] = useState({
+        hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
+        minutes: ('0'+date.getMinutes()).slice(-2),
+        seconds: ('0'+date.getSeconds()).slice(-2)
+    });
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setState({
+                hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
+                minutes: ('0'+date.getMinutes()).slice(-2),
+                seconds: ('0'+date.getSeconds()).slice(-2)
+            })
+        }, 1000)
 
+        return () => clearInterval(interval);
+    })
+    
 
     return (
-            <Clock
-                message={'ex05: useEffect Hook example'}
-                hours={10}
-                minutes={25}
-                seconds={40}/>
+        <Clock
+            message={'ex05: useEffect Hook example'}
+            hours={state.hours}
+            minutes={state.minutes}
+            seconds={state.seconds}/>
     );
+
 }
