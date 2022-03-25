87f0498846def73ab5a5a09cc4b533808e690220
Wonseok <ls4050@naver.com>
2022-03-25 11:00:35 +0900

    frontend

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,50 +1,51 @@
-import React,{ useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import './assets/scss/App.scss';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
 
 const App = () => {
-    const [emails, setEmails] = useState([]);
+  const [emails, setEmails] = useState([]);
 
-    useEffect(async () => {
-        const response = await fetch('/api', {
-            method: 'get',
-            headers: {
-                'Content-Type' : 'application/json',
-                'Accept' : 'application/json'
-            },
-            body: null
-        });
+  useEffect(async () => {
+      const response = await fetch('/api', {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: null
+      });
 
-        if(!response.ok){
-            console.log("error:", response.status, response.statusText);
-            return;
-        }
+      if(!response.ok) {
+        console.log("error:", response.status, response.statusText);
+        return;
+      }
 
-        const json = await response.json();
+      const json = await response.json();
 
-        if(json.result !== 'success') {
-            console.log("error:", json.meesage);
-            return;
-        }
+      if(json.result !== 'success') {
+        console.log("error:", json.message);
+        return;      
+      }
+      
+      setEmails(json.data);
 
-        setEmails(json.data);
+  }, []);
 
-    }, []);
 
-    const notifyKeywordChange = (kw) => {
-        console.log("/api?kw="+kw);
-        // setEmails(data.filter(e => e.firstName.indexOf(kw) != -1 || e.lastName.indexOf(kw) != -1 || e.email.indexOf(kw) != -1));
-    }
+  const notifyKeywordChange = function(keyword) {
+    console.log("/api?kw=" + keyword);
+    // setEmails(data.filter(e => e.firstName.indexOf(keyword) != -1 || e.lastName.indexOf(keyword) != -1 || e.email.indexOf(keyword) != -1));
+  }
 
-    return (
-        <div className={'App'}>
-            <RegisterForm />
-            <SearchBar callback={notifyKeywordChange} />
-            <Emaillist emails={emails}/>
-        </div>
-    )
+  return (
+    <div className={'App'}>
+      <RegisterForm />
+      <SearchBar callback={notifyKeywordChange}/>
+      <Emaillist emails={emails} />
+    </div>
+  )
 }
 
 export default App
