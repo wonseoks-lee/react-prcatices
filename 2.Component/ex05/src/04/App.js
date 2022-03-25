16b4771494b1103eef9fc0f4551b51c906c21b90
Wonseok <ls4050@naver.com>
2022-03-25 10:59:32 +0900

    Clock 수정

--- 2.Component/ex05/src/04/App.js
+++ 2.Component/ex05/src/04/App.js
@@ -3,33 +3,51 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
-    const date = new Date();
-    const hours = date.getHours();
-    const [state, setState] = useState({
-        hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
-        minutes: ('0'+date.getMinutes()).slice(-2),
-        seconds: ('0'+date.getSeconds()).slice(-2)
-    });
+    const getCurrentClockTime = () =>{
+        const date = new Date();
+        console.log("render render")
+        const hours = date.getHours();
+        return {
+            hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
+            minutes: ('0'+date.getMinutes()).slice(-2),
+            seconds: ('0'+date.getSeconds()).slice(-2),
+            session: hours > 12 ? "pm" : "am"
+        };
+    }
+
+    const [currentClockTime, setCurrentClockTime] = useState(getCurrentClockTime());
+    const [ticks, setTicks] = useState(0);
+    
+    //  { , []} => 한번만 실행되는 것을 보장 
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setCurrentClockTime(getCurrentClockTime());
+    //         setTicks(ticks + 1);
+    //     });
+    // }, [])
     
     useEffect(() => {
-        const interval = setInterval(() => {
-            setState({
-                hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
-                minutes: ('0'+date.getMinutes()).slice(-2),
-                seconds: ('0'+date.getSeconds()).slice(-2)
-            })
-        }, 1000)
-
-        return () => clearInterval(interval);
-    })
+        console.log('Update Color(DB) Using APIs...');
+        setTimeout(() => {
+            setCurrentClockTime(getCurrentClockTime());
+            setTicks(ticks + 1);
+            console.log("ticktictick ==> ", ticks)
+        }, 1000);
+        console.log("---------------------------------------------------")
+    }, [currentClockTime])
     
 
+
+
     return (
+        <>
+        <span>{ticks}</span>
         <Clock
             message={'ex05: useEffect Hook example'}
-            hours={state.hours}
-            minutes={state.minutes}
-            seconds={state.seconds}/>
+            hours={currentClockTime.hours}
+            minutes={currentClockTime.minutes}
+            seconds={currentClockTime.seconds}/>
+        </>
     );
 
 }
