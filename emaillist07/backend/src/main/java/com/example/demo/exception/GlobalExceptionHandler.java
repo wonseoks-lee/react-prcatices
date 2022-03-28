5a951b59a0e1b635c0169ec0dfc623c6d4edd3fb
Wonseok <ls4050@naver.com>
2022-03-28 10:57:32 +0900

    kanbanbuild

--- emaillist07/backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ emaillist07/backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -4,8 +4,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,8 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
-	
 	
 	@ExceptionHandler(Exception.class)
 	@ResponseBody
@@ -28,7 +24,7 @@ public class GlobalExceptionHandler {
 		//1. 로깅
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		LOGGER.error(errors.toString());
+		log.error(errors.toString());
 			
 		//2. JSON 응답
 		return ResponseEntity.status(HttpStatus.OK).body(JsonResult.fail(errors.toString()));
---
+++ kanbanboard07/backend/src/main/java/com/example/demo/Kanbanboard07Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kanbanboard07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kanbanboard07Application.class, args);
+	}
+
+}
