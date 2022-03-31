0ae0969081624a5ec7f191bcf3a4f906f7b24150
Wonseok <ls4050@naver.com>
2022-03-31 09:53:50 +0900

    hash routing build

---
+++ 3.routing/src/01/component/Guestbook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Guestbook() {
+    return (
+        <div>
+            <ul>
+                <li><a href='/#/'>main</a></li>
+                <li>guestbook</li>
+                <li><a href='/#/gallery'>gallery</a></li>
+            </ul>
+            <h1>Guestbook</h1>
+        </div>
+    );
+}
